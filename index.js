e47da42d5dadba6e088b36bf1a63922d913aca4f
vahabdk <varo10ab@student.cbs.dk>
2020-04-02 10:18:04 +0200

    changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require("express"); //Henter express
+const express = require('express'); //Henter express
 const app = new express(); // kalder den app, for at gøre det nemmere at bruge senere
 const ejs = require('ejs'); //Anvender ejs engine
 const mongoose = require('mongoose'); //For at kunne forbinde til databasen skal mongoose hentes
-const bodyParser = require("body-parser"); // For at kunne lave middleware
-mongoose.connect("mongodb://localhost/bookingSystemDb",{useNewUrlParser:true}); /*Der skabes forbindelse til databasen */
+const bodyParser = require('body-parser'); // For at kunne lave middleware
+mongoose.connect('mongodb://localhost/bookingSystemDb',{useNewUrlParser:true}); /*Der skabes forbindelse til databasen */
 app.use(express.static('public'))
+app.set('view engine','ejs')
+
 
 app.listen(2000, ()=>{
     console.log('Klar til at booke møder')
