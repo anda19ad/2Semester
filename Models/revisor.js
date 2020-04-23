03026a74e8306c20cb34e14b45213053ebff4db3
ahellow <ahellow@users.noreply.github.com>
2020-04-23 14:18:22 +0200

    jj

--- Models/revisor.js
+++ Models/revisor.js
@@ -3,40 +3,43 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Definerer modellen. Datastrukturen.
-const revisorSchema = new Schema({
-    Fornavn: {
-        type: String,
-        required: true,
-    },
-    Efternavn: {
-        type: String,
-        required: true,
-    },
-    Email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    Tlf: {
-        type: Number,
-        unique: true,
-        required: true,
-    },
-    startTime:Number,
-    slutTime:Number,
-    startMinut:Number,
-    slutMinut:Number,
-    Brugernavn: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    Kodeord: {
-        type: String,
-        required: true,
-    }
-});
+
+    const revisorSchema = new Schema({
+        Fornavn: {
+            type: String,
+            required: true,
+        },
+        Efternavn: {
+            type: String,
+            required: true,
+        },
+        Email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        Tlf: {
+            type: Number,
+            unique: true,
+            required: true,
+        },
+        startTime: Number,
+        slutTime: Number,
+        startMinut: Number,
+        slutMinut: Number,
+        Brugernavn: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        Kodeord: {
+            type: String,
+            required: true,
+        }
+    });
+
 
 //Gør det muligt at gemme revisorer s. 95
 const Revisor = mongoose.model('Revisor',revisorSchema);
 module.exports = Revisor;
+
