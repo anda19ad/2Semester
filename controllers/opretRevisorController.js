d97f036b6a2f1d226ac8b5bb282bdbd165232f43
vahabdk <varo10ab@student.cbs.dk>
2020-04-23 12:25:09 +0200

    a

--- controllers/opretRevisorController.js
+++ controllers/opretRevisorController.js
@@ -1,3 +1,13 @@
+const Afdeling = require ('../Models/afdeling');
+
+module.exports = async(req,res)=>{
+    const afdelings = await Afdeling.find({});
+    res.render('opretRevisor',{
+        afdelings
+    });
+};
+/*
 module.exports=(req,res)=>{
     res.render('opretRevisor') //Henter opretRevisor.ejs
 }
+*/
